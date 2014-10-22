279c18eee5573f4b60a1071900c464ec053956c2
Edward Yang <yushine999@gmail.com>
2014-10-22 10:14:20 +0800

    Fixed issue for spree 1.3.6 and fetch sale_events for sidebar.

--- app/controllers/spree/active_sales_controller.rb
+++ app/controllers/spree/active_sales_controller.rb
@@ -1,6 +1,7 @@
 module Spree
   class ActiveSalesController < Spree::StoreController
     before_filter :load_sale, :except => :index
+    before_filter :load_sale_events
     rescue_from ActiveRecord::RecordNotFound, :with => :render_404
     helper 'spree/taxons'
     helper 'spree/products'
@@ -8,10 +9,6 @@ module Spree
     respond_to :html
 
     def index
-      @searcher = Config.searcher_class.new(params)
-      @searcher.current_user = try_spree_current_user
-      @searcher.current_currency = current_currency
-      @sale_events = @searcher.retrieve_sales
       respond_with(@sale_events)
     end
 
@@ -31,7 +28,8 @@ module Spree
         end
       end
 
-      respond_with(@sale_event)
+      # respond_with(@sale_event)
+      render :layout => 'responsive_application'
     end
 
     private
@@ -39,8 +37,15 @@ module Spree
         @sale_event ? @sale_event.name : super
       end
 
+      def load_sale_events
+        @searcher = Config.searcher_class.new(params)
+        @searcher.current_user = try_spree_current_user
+        @searcher.current_currency = current_currency
+        @sale_events = @searcher.retrieve_sales
+      end
+
       def load_sale
-        @active_sale = ActiveSale.find_by_permalink!(params[:id])
+        @active_sale = Spree::ActiveSale.find_by_permalink!(params[:id])
         @sale_event = @active_sale.active_sale_events.live_active_and_hidden(:hidden => false).first 
         if @sale_event.present?
           @sale_properties, @products = @sale_event.sale_properties.includes(:property), @sale_event.products
