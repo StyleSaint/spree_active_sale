abd3b3e0daba49d2b0d99c695036df8dae956dc2
Edward Yang <yushine999@gmail.com>
2014-10-30 11:32:03 +0800

    Fixed issue of count down timer.

--- vendor/assets/javascripts/spree_active_sale/event_timer.js
+++ vendor/assets/javascripts/spree_active_sale/event_timer.js
@@ -2,19 +2,12 @@ $(document).ready(function() {
   $("[data-timer]").each(function() {
     var cTime = $(this).attr('data-timer');
     var timeLayout = $(this).attr('data-layout');
-    // endTime for event in db:
+    // endTime for event in db and should be with timezone, like 2014-10-30T05:04:00-0700:
     var endTime = new Date(cTime);
-    // Return the timezone difference between UTC and User Local Time
-    // var date = new Date();
-    var userTimeZoneDiff = endTime.getTimezoneOffset();
-    // Since there are 60,000 milliseconds in a minute
-    var MS_PER_MINUTE = 60000;
-    // Event's final end date will depend on the final subtracted date as:
-    var eventEndDate = new Date(endTime - userTimeZoneDiff * MS_PER_MINUTE);
 
-    $(this).countdown({ 
-      until: eventEndDate, 
-      compact: true, 
+    $(this).countdown({
+      until: endTime,
+      compact: true,
       layout: timeLayout
     });
   });
